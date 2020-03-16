42d22e96cfa3833c963cc7030ec9617decd63aba
Rusifikus <ruslanroslichenko@gmail.com>
2020-03-16 22:46:35 +0200

    Yoga

---
+++ Yoga/js/script.js
@@ -0,0 +1,106 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for(let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+
+    });
+
+    // Timer
+
+    let deadline = '2020-03-17';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total' : t,
+            'seconds' : seconds,
+            'minutes' : minutes,
+            'hours' : hours
+        }
+    }
+
+        function setClock (id, endtime) {
+            let timer = document.getElementById(id),
+            seconds = timer.querySelector('.seconds'),
+            minutes = timer.querySelector('.minutes'),
+            hours = timer.querySelector('.hours'),
+            timeInterval = setInterval(updateClock, 1000);
+
+            function updateClock () {
+                let t = getTimeRemaining(endtime);
+
+                function addZero(num) {
+                    if (num <= 9) {
+                        return '0' + num;
+                    } else return num;
+                };
+
+                hours.textContent = addZero(t.hours),
+                minutes.textContent = addZero(t.minutes),
+                seconds.textContent = addZero(t.seconds);
+
+                if (t.total <= 0) {
+                    clearInterval(timeInterval);
+                    hours.textContent = '00',
+                    minutes.textContent = '00',
+                    seconds.textContent = '00';
+                }
+            }
+        }
+
+    setClock('timer', deadline);
+
+    // modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+
+        more.addEventListener('click', function () {
+            overlay.style.display = 'block';
+            this.classList.add('more-splah');
+            document.body.style.overflow = 'hidden';
+        })
+    
+        close.addEventListener('click', function () {
+            overlay.style.display = 'none';
+            more.classList.remove('more-splah');
+            document.body.style.overflow = ('');
+        })
+
+});
