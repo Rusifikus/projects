bbb75079822fd49ba3948a0aa266bd48e25abe13
Rusifikus <ruslanroslichenko@gmail.com>
2020-03-28 12:08:30 +0200

    upload

---
+++ MoGo/js/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', ()=> {
+
+    let accordionTitle = document.querySelectorAll('.whatwedo_accordion_item'),
+        accordionText = document.querySelectorAll('.accordion_text');
+
+
+});
--- Yoga/js/script.js
+++ Yoga/js/script.js
@@ -37,7 +37,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Timer
 
-    let deadline = '2020-03-25';
+    let deadline = '2020-03-30';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -198,5 +198,50 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     })
+
+
+    // calc 
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+        totalValue.innerHTML = 0;
+
+        persons.addEventListener('input', function () {
+            personsSum = +this.value;
+            total = (daysSum + personsSum)*4000;
+
+            if(restDays.value == '' || persons.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total;
+            }
+
+        });
+
+        restDays.addEventListener('input', function () {
+            daysSum = +this.value;
+            total = (daysSum + personsSum)*4000;
+
+            if(persons.value == '' || restDays.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total;
+            }
+        });
+
+        place.addEventListener('change', function () {
+            if(persons.value == '' || restDays.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                let a = total;
+                totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+            }
+        });
 
 });
